737503e702686627122b299f4c3fd37d2724db29
Vanya Pavlova <vanya.pavlova@gmail.com>
2018-07-31 15:17:20 +0300

    Added basic elements to the DOM + toHtmlString of To-Do object

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
 import '../sass/style.scss';
 import {Todo} from './todo.js';
 
-const t1 = new Todo({ value: 'test1', completed: true });
-const t2 = new Todo({ value: 'test2', completed: false });
+const t1 = new Todo({ value: 'To-Do-Item-1', completed: true });
+const t2 = new Todo({ value: 'To-Do-Item-2', completed: false });
+const t3 = new Todo({ value: 'To-Do-Item-3', completed: true });
+const t4 = new Todo({ value: 'To-Do-Item-4', completed: false });
+const t5 = new Todo({ value: 'To-Do-Item-5', completed: true });
+const t6 = new Todo({ value: 'To-Do-Item-6', completed: false });
 
-console.log(t1,t2);
+const list = [t1,t2,t3,t4,t5,t6];
+
+const ui_list = document.getElementById("js-todo-list");
+const html = list.map((item) => item.toHtmlString()).join('');
+
+console.log(html);
+ui_list.innerHTML= html;
+
--- src/js/todo.js
+++ src/js/todo.js
@@ -3,4 +3,16 @@ export class Todo {
      this.value = value;
      this.completed = completed;
    }
+
+   toHtmlString(props) {
+     return `
+      <li>
+        <label>
+        	<input type="checkbox" ${this.completed ? 'checked' : ''}>
+            ${this.value}
+        </label>
+        <button type="button" data-action="edit">Edit</button>
+        <button type="button" data-action="delete">Delete</button>
+      </li>`;
+   }
 }

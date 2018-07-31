a897d726434a503202623b1961d77ca87dad0e36
Vanya Pavlova <vanya.pavlova@gmail.com>
2018-07-31 23:42:34 +0300

    Edit Item implementation

--- src/js/event-handlers.js
+++ src/js/event-handlers.js
@@ -2,6 +2,7 @@ export class TodoListEventHandler {
 
   static delegatedTodoItemClick(event, todoList) {
     event.preventDefault();
+    console.log(event.target);
 
     const target = event.target;
     const nodeName = event.target.nodeName;
@@ -9,7 +10,7 @@ export class TodoListEventHandler {
     if (nodeName === 'BUTTON') {
       // debugger;
       const action = target.dataset.action;
-      const li = target.parentElement;
+      const li = target.parentElement.parentElement;
       const index = Number(li.dataset.index);
 
       if (action === 'delete') {
@@ -18,13 +19,28 @@ export class TodoListEventHandler {
          todoList.saveToLocalStorage();
       }
 
-      if (action === 'edit') {
-          window.alert('edit');
+      if (action === 'edit' || action === 'cancel') {
+          li.querySelector('.item-default-view').classList.toggle('is-hidden');
+          li.querySelector('.item-edit-view').classList.toggle('is-hidden');
+          li.querySelector('.js-edit-input').focus();
+      }
+
+      if (action === 'save') {
+         const value = li.querySelector('.js-edit-input').value;
+         todoList.editItem(index, value);
+         todoList.render();
+         todoList.saveToLocalStorage();
       }
     }
     
+    if (nodeName === 'INPUT') {
+       if (target.classList.contains('js-edit-input')) {
+         return;
+       }
+    }
+
     if (nodeName === 'INPUT' || nodeName === 'LABEL') {
-        const li = target.parentElement;
+        const li = target.parentElement.parentElement;
         const index = Number(li.dataset.index);
         todoList.toggleItem(index);
         todoList.render();
--- src/js/todo.js
+++ src/js/todo.js
@@ -7,10 +7,17 @@ export class Todo {
    toHtmlString(props) {
      return `
       <li data-index="${props.index}" data-completed="${this.completed}">
-        <input type="checkbox" id="cc" ${this.completed ? 'checked' : ''}>
-        <label for="cc">${this.value}</label>
-        <button type="button" data-action="delete" class="btn btn-danger">✖</button>
-        <button type="button" data-action="edit" class="btn btn-warning">✎</button>
+        <div class="item-default-view clearfix">
+          <input type="checkbox" id="cc" ${this.completed ? 'checked' : ''}>
+          <label for="cc">${this.value}</label>
+          <button type="button" data-action="delete" class="btn btn-danger pull-right">✖</button>
+          <button type="button" data-action="edit" class="btn btn-warning pull-right">✎</button>
+        </div>
+        <aside class="item-edit-view clearfix is-hidden">
+          <input type="text" class=" js-edit-input form-control pull-left" value="${this.value}"/>
+          <button type="button" data-action="cancel" class="btn btn-warning pull-right">✖</button>
+          <button type="button" data-action="save" class="btn btn-success pull-right">✔</button>
+        </aside>
       </li>`;
    }
 }
